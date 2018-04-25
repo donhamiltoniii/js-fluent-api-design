67e4ab36b0e3b084675efc2c7d1ce14ec690e56b
Don Hamilton III <donhamiltoniii@gmail.com>
2018-04-25 18:48:53 -0400

    INITIAL COMMITy

---
+++ index.js
@@ -0,0 +1,19 @@
+var myDOM = (function() {
+	function myDOM() {}
+
+	myDOM.prototype._gimme = function(element) {
+		if (document.querySelectorAll(element).length > 1)
+			this.element = document.querySelectorAll(element)
+		else if (document.querySelector(element))
+			this.element = document.querySelector(element)
+		else this.element = document.createElement(element)
+		return this
+	}
+
+	myDOM.prototype.addClass = function(className) {
+		this.element.classList.add(className)
+		return this
+	}
+
+	return myDOM
+})()
